6847526b09a8f11c6e7ae3f752c9bf58036cbb56
Vijay Mahantesh SM <vijaym123@gmail.com>
2013-10-17 00:05:38 -0500

    Added more code to iterate through different congress. I.E 111,112,113

--- scrapeData.py
+++ scrapeData.py
@@ -1,22 +1,21 @@
 import json
 import urllib
 
-congress = 111
-jsonDump = {}
-filename = str(congress)+"_house_bills.txt"
-offset=0
+congressList = [111,112,113]
 
-f = open(filename,"w")
-test = urllib.urlopen("http://www.govtrack.us/api/v2/bill?congress="+str(congress)+"&"+"offset="+str(offset))
-jsonDump = json.loads(test.read())
-	
-while (jsonDump["objects"]):
-	offset += 1
-	print jsonDump["meta"]["offset"], jsonDump["meta"]["total_count"]
-	for obj in jsonDump['objects']:
-		f.write(str(congress)+","+obj["bill_type"]+","+str(obj["id"])+","+str(obj["sliplawpubpriv"])+"\n")
+for congress in congressList:
+	jsonDump = {}
+	filename = str(congress)+"_house_bills.txt"
+	offset=0
+	f = open(filename,"w")
 	test = urllib.urlopen("http://www.govtrack.us/api/v2/bill?congress="+str(congress)+"&"+"offset="+str(offset))
-	jsonDump = json.loads(test.read())	
-f.close
-
-	
+	jsonDump = json.loads(test.read())
+			
+	while (jsonDump["objects"]):
+		offset += 1
+		print jsonDump["meta"]["offset"], jsonDump["meta"]["total_count"]
+		for obj in jsonDump['objects']:
+			f.write(str(congress)+","+obj["bill_type"]+","+str(obj["id"])+","+str(obj["sliplawpubpriv"])+"\n")
+		test = urllib.urlopen("http://www.govtrack.us/api/v2/bill?congress="+str(congress)+"&"+"offset="+str(offset))
+		jsonDump = json.loads(test.read())	
+	f.close()
